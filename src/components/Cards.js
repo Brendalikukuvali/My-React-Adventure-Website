713c3c498be74fc91537b1119b9d55176d5faddd
Brendalikukuvali <brendalikukuvali@gmail.com>
2020-12-15 21:08:54 +0300

    commit message

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,14 +10,14 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/imageA.jpg'
-              text='Explore the hidden deep and beautiful waterfalls'
+              src='images/Falls.jpg'
+              text='Explore the hidden deep and beautiful Fourteen falls in Thika Kenya'
               label='Adventure'
               path='/services'
             />
             <CardItem
               src='images/imageB.jpg'
-              text='Travel through and get a chance to have an amazing moment at the beach'
+              text='Travel through and get a chance to have an amazing moment at the beach in Mombasa'
               label='Luxury'
               path='/services'
             />
@@ -30,14 +30,56 @@ function Cards() {
               path='/services'
             />
             <CardItem
-              src='images/imageD.jpg'
-              text='Experience the cool climate of different Islands'
+              src='images/Island.jpg'
+              text='Experience the cool climate of the Rusinga Islands'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/imageC.jpg'
-              text='Ride through the Desert on a guided camel tour'
+              src='images/Chalbi.jpg'
+              text='Ride through the Chalbi Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+          
+          <ul className='cards__items'>
+            <CardItem
+              src='images/Lion.jpg'
+              text='Visit the Maasai Mara National Park to see the big five animals'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/Mount.jpg'
+              text='You will have a chance to see The Mount Kenya Trek'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/Stone.jpg'
+              text='Visit the Crying Stone in Kakamega county'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+
+          <ul className='cards__items'>
+            <CardItem
+              src='images/Springs.jpg'
+              text='Having fun visiting hot Springs of Lake Bogoria'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/Rift.jpg'
+              text='Get to experience the Great Rift Valley'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/Uhuru.jpg'
+              text='The fascinating Uhuru Gardens located in Nairobi'
               label='Adrenaline'
               path='/sign-up'
             />
